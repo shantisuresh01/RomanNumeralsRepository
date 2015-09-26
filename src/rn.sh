b71244bd1bfb6492f1f8fde55e94d4fbf89e5ae3
shantisuresh01 <shantisuresh01@gmail.com>
2015-09-26 11:10:32 -0400

    Changed rn.sh to include git workflow commands

--- src/rn.sh
+++ src/rn.sh
@@ -2,3 +2,12 @@
 
 javac -cp /usr/share/java/lib/junit-4.12.jar Roman.java RomanTest.java
 java -ea -cp .:/usr/share/java/lib/*  org.junit.runner.JUnitCore  RomanTest
+
+# git add .
+# git commit -m 
+# git push origin dev
+# Merge with master:
+# git fetch origin master
+# git merge master
+# git push origin dev:master
+
