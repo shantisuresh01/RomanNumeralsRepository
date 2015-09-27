040dd9e65eebe2d4449eb72d840a9dcb50a467e3
shantisuresh01 <shantisuresh01@gmail.com>
2015-09-26 21:48:13 -0400

    Mavenized RomanNumerals project - added pom.xml.  In pom.xml: added surefire plugin and junit 4.12 dependency; added classpath for junit in pom.xml.

---
+++ rn.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Create a Maven project:
+# Run Tests:
+mvn test
+# Compile source and create a WAR file to later install in Tomcat perhaps.
+mvn clean compile war:war
+
+# Commenting the following because project has been properly Mavenized.
+# javac -cp /usr/share/java/lib/junit-4.12.jar Roman.java RomanTest.java
+# java -ea -cp .:/usr/share/java/lib/*  org.junit.runner.JUnitCore  RomanTest
+
+# git add .
+# git commit -m 
+# git push origin dev
+# Merge with master:
+# git fetch origin master
+# git merge master
+# git push origin dev:master
+
